4804e8d1f28afece6beb9111a9cad53d4b8a9e9c
acaporrini <alessandro.caporrini@daliaresearch.com>
2018-07-29 12:15:22 +0200

    Adding delay for search

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash'
 const API_KEY = "AIzaSyAVOXghAI53QHh_ntZtspcdanBoMhztSmE";
 
 
@@ -16,7 +17,7 @@ class App extends Component {
       selectedVideo: null
      };
 
-     this.videoSearch('surfboards')
+     this.videoSearch('yoga')
   }
   videoSearch(term){
     YTSearch({key: API_KEY, term: term}, (videos) => {
@@ -27,9 +28,10 @@ class App extends Component {
     });
   }
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar onSearchTermChange={ term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={ videoSearch } />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
